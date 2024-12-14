9b5734a63660b3170e6abff984e929e3115bb33b
misterbabu281@gmail.com <misterbabu281@gmail.com>
2024-12-15 02:27:52 +0530

    first commit

---
+++ src/main/java/com/jocata/service/DemoSpringJenkins.java
@@ -0,0 +1,23 @@
+package com.jocata.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class DemoSpringJenkins {
+    public static Logger logger = LoggerFactory.getLogger(DemoSpringJenkins.class);
+
+    @PostConstruct
+    public void inti() {
+        logger.info("Application started....");
+    }
+
+    public static void main(String[] args) {
+        logger.info("Application executed....");
+        SpringApplication.run(DemoSpringJenkins.class, args);
+    }
+}
